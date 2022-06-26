52caf00aa9fe662fca3a99a5e81e1b4c94288ed5
mutemip <paulmutemi2030@gmail.com>
2022-06-27 02:05:29 +0300

    fetching specific element practice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm,addEventListener('submit', e => {
+        e.preventDefault();
+        const input = document.querySelector('input#searchByID')
+        
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then (res => res.json())
+        .then (movies => {
+            const title = document.querySelector("section#movieDetails h4")
+            const summary = document.querySelector("section#movieDetails p")
+
+            title.textContent = movies.title;
+            summary.textContent = movies.summary;
+        })
+        inputForm.reset();
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
